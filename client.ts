0fb653b44b912d8cf0bb41e430fa9ca2a61eb586
Adrian Sampson <adrian@radbox.org>
2018-09-03 15:19:01 -0400

    Set up WebSocket connection

--- client.ts
+++ client.ts
@@ -1,1 +1,14 @@
-console.log("hi");
+import * as WebSocket from 'ws';
+import * as querystring from 'querystring';
+
+const WS_URL = 'wss://api.campuswire.com/v1/ws';
+
+function connect(token: string) {
+  let ws_url = WS_URL + '?' + querystring.stringify({
+    'access_token': token,
+    'v': '1',
+  });
+  let socket = new WebSocket(ws_url);
+}
+
+connect(process.argv[2]);
