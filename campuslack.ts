20064ac29132a157066322e72608c7c16c4b7f10
Adrian Sampson <adrian@radbox.org>
2018-09-08 16:29:33 -0400

    Filter by group ID

--- campuslack.ts
+++ campuslack.ts
@@ -1,19 +1,26 @@
 import { CWClient } from './cwclient';
 
-let cw_token = process.env['CAMPUSWIRE_TOKEN'];
-if (!cw_token) {
-  throw "set CAMPUSWIRE_TOKEN";
+function forceEnv(key: string): string {
+  let value = process.env[key];
+  if (!value) {
+    throw "set ${key}";
+  }
+  return value;
 }
+
+let cw_token = forceEnv('CAMPUSWIRE_TOKEN');
+let cw_groupid = forceEnv('CAMPUSWIRE_GROUP');
+
 let client = new CWClient(cw_token);
 client.connect();
 
 client.on('wall-post-created', data => {
   let post = data.post;
-  if (!post.draft) {
+  if (post.group === cw_groupid && !post.draft) {
     console.log(post.title);
     console.log(post.group);
     console.log(post.body);
-    console.log(post.note);
+    console.log(post.type);
     console.log(post);
   }
 });
