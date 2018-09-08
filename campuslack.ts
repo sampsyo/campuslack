eec9b769ebc575e052f92517e767617b7fe71e27
Adrian Sampson <adrian@radbox.org>
2018-09-08 17:05:30 -0400

    Post messages to Slack

--- campuslack.ts
+++ campuslack.ts
@@ -1,4 +1,5 @@
 import { CWClient } from './cwclient';
+import fetch from 'node-fetch';
 
 function forceEnv(key: string): string {
   let value = process.env[key];
@@ -8,9 +9,26 @@ function forceEnv(key: string): string {
   return value;
 }
 
+/**
+ * Send a JSON payload to a Slack Incoming Webhook URL.
+ */
+async function slackHook(url: string, data: any) {
+  let res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+  if (!res.ok) {
+    console.error(`Slack hook error: ${res.statusText}`);
+  }
+}
+
 async function main() {
   let cw_token = forceEnv('CAMPUSWIRE_TOKEN');
   let cw_groupid = forceEnv('CAMPUSWIRE_GROUP');
+  let slack_hookurl = forceEnv('SLACK_HOOK_URL');
 
   let client = new CWClient(cw_token);
 
@@ -29,9 +47,16 @@ async function main() {
       let courseSlug = groups[cw_groupid].slug;
       let url = `https://campuswire.com/c/${courseSlug}/feed/${post.slug}`;
 
-      console.log(`New ${post.type}: ${post.title}`);
-      console.log(url);
-      console.log(post.body);
+      // Craft a message to post to Slack.
+      let message = `New ${post.type}: ${post.title}\n`;
+      message += url + '\n';
+      message += post.body;
+
+      // Send the message.
+      console.log(message);
+      slackHook(slack_hookurl, {
+        text: message,
+      });
     }
   });
 }
