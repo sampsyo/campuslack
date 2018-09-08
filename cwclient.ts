645bcb8fef534434afbf8f69ce33a0e64090eaa8
Adrian Sampson <adrian@radbox.org>
2018-09-08 13:54:43 -0400

    Client class

--- cwclient.ts
+++ cwclient.ts
@@ -3,21 +3,28 @@ import * as querystring from 'querystring';
 
 const WS_URL = 'wss://api.campuswire.com/v1/ws';
 
-function connect(token: string) {
-  let ws_url = WS_URL + '?' + querystring.stringify({
-    'access_token': token,
-    'v': '1',
-  });
-  let socket = new WebSocket(ws_url);
+class CWClient {
+  constructor(
+    public token: string
+  ) {}
 
-  socket.on('message', data => {
-    let msg = JSON.parse(data.toString());
-    console.log(msg);
-  });
+  connect() {
+    let ws_url = WS_URL + '?' + querystring.stringify({
+      'access_token': this.token,
+      'v': '1',
+    });
+    let socket = new WebSocket(ws_url);
+  
+    socket.on('message', data => {
+      let msg = JSON.parse(data.toString());
+      console.log(msg);
+    });
+  }
 }
 
 let cw_token = process.env['CAMPUSWIRE_TOKEN'];
 if (!cw_token) {
   throw "set CAMPUSWIRE_TOKEN";
 }
-connect(cw_token);
+let client = new CWClient(cw_token);
+client.connect();
