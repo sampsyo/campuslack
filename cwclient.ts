4db3a4cee13ec9b47cd782c2956ecfd9536c578c
Adrian Sampson <adrian@radbox.org>
2018-09-09 11:12:54 -0400

    Add lots of logging

--- cwclient.ts
+++ cwclient.ts
@@ -5,6 +5,7 @@ import { EventEmitter } from 'events';
 
 const WS_URL = 'wss://api.campuswire.com/v1/ws';
 const API_BASE = 'https://api.campuswire.com/v1/';
+const RECONNECT_INTERVAL = 2000;  // Two seconds.
 
 export interface CWEvent {
   event: string;
@@ -27,10 +28,30 @@ export class CWClient extends EventEmitter {
       'v': '1',
     });
     let socket = new WebSocket(ws_url);
-  
+
+    socket.on('open', () => {
+      console.log("connected");
+    });
+
+    // Receive event messages.
     socket.on('message', data => {
       let event = JSON.parse(data.toString()) as CWEvent;
+      console.log("received event", event.event);
       this.emit(event.event, event.data);
+    });
+
+    // Log errors.
+    socket.on('error', err => {
+      console.error("socket error", err);
+    });
+
+    // Reconnect when disconnected.
+    socket.on('close', (code, reason) => {
+      console.log(`socket closed (${code}): ${reason}`);
+      setTimeout(() => {
+        console.log("reconnecting");
+        this.connect();
+      }, RECONNECT_INTERVAL);
     });
   }
 
